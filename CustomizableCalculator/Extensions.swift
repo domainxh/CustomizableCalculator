3b75ca439de55d4e38a9f875e3f974f2a6243e9a
Soja <domain.xh@gmail.com>
2017-02-09 20:46:32 -0500

    Calculator functionality implimented

---
+++ CustomizableCalculator/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  CustomizableCalculator
+//
+//  Created by Xiaoheng Pan on 2/9/17.
+//  Copyright © 2017 Xiaoheng Pan. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.characters.index(self.startIndex, offsetBy: i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+
+    subscript (r: Range<Int>) -> String {
+        return substring(with: characters.index(startIndex, offsetBy: r.lowerBound) ..< characters.index(startIndex, offsetBy: r.upperBound))
+    }
+}
+
---
+++ CustomizableCalculator/StorageVC.swift
@@ -0,0 +1,21 @@
+//
+//  StorageVC.swift
+//  CustomizableCalculator
+//
+//  Created by Xiaoheng Pan on 2/9/17.
+//  Copyright © 2017 Xiaoheng Pan. All rights reserved.
+//
+
+import UIKit
+
+class StorageVC: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
