fb04d350bb1f8eecf6f93fbe2ee8ec91e539108d
Soja <domain.xh@gmail.com>
2017-02-11 21:24:16 -0500

    UserDefault implimented, Done with the calculator portion.

--- CustomizableCalculator/Extensions.swift
+++ CustomizableCalculator/Extensions.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension String {
     
@@ -23,3 +24,75 @@ extension String {
     }
 }
 
+//extension UserDefaults {
+//    func setPassword(password: String) {
+//        set(password, forKey: "password")
+//        synchronize()
+//    }
+//    
+//    func isPasswordSet() -> Bool {
+//        return bool(forKey: "isPasswordSet")
+//    }
+//    
+//}
+
+extension UIColor {
+    static func rgb(_ red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
+let imageCache = NSCache<NSString, UIImage>()
+
+class CustomImageView: UIImageView {
+    
+    var imageUrlString: String?
+    
+    func loadImageUsingUrlString(_ urlString: String) {
+        
+        imageUrlString = urlString
+        
+        let url = URL(string: urlString)
+        
+        image = nil
+        
+        if let imageFromCache = imageCache.object(forKey: urlString as NSString) {
+            self.image = imageFromCache
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, respones, error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                
+                let imageToCache = UIImage(data: data!)
+                
+                if self.imageUrlString == urlString {
+                    self.image = imageToCache
+                }
+                
+                imageCache.setObject(imageToCache!, forKey: urlString as NSString)
+            })
+            
+        }).resume()
+    }
+    
+}
--- CustomizableCalculator/StorageVC.swift
+++ CustomizableCalculator/StorageVC.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class StorageVC: UIViewController {
 
+    var isMenuShowing = false
+    @IBOutlet weak var slideMenuLeadingConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        UINavigationBar.appearance().barTintColor = UIColor.darkGray
+        navigationItem.leftBarButtonItem?.tintColor = UIColor.white
+
     }
 
+    @IBAction func slideMenuTapped(_ sender: Any) {
+        if isMenuShowing {
+            slideMenuLeadingConstraint.constant = -140
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        } else {
+            slideMenuLeadingConstraint.constant = 0
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+            
+        }
+        isMenuShowing = !isMenuShowing
+    }
 
 }
