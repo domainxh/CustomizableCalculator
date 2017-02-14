7dcfa659cff8d594d9c873ec96f1a7f92ba9d1f0
Soja <domain.xh@gmail.com>
2017-02-14 14:48:37 -0500

    added iTunesharing capability, and photo parsing

--- CustomizableCalculator/MediaCell.swift
+++ CustomizableCalculator/MediaCell.swift
@@ -12,16 +12,39 @@ class MediaCell: UICollectionViewCell {
     
     @IBOutlet weak var cellImage: UIImageView!
     
-    override func awakeFromNib() {
-        layer.cornerRadius = 5
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.darkGray.cgColor
-        clipsToBounds = true
+//    override func awakeFromNib() {
+//        layer.cornerRadius = 5
+//        layer.borderWidth = 2
+//        layer.borderColor = UIColor.darkGray.cgColor
+//        clipsToBounds = true
+//    }
+
+    func configureCell(URL: String) {
+        cellImage.image = UIImage(contentsOfFile: URL)
     }
     
-    
-    func configureCell() {
-        
-    }
+//    func configureCell(fileName: String) {
+//        var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+//        let filePath = NSURL(fileURLWithPath: documentsDirectory).appendingPathComponent(fileName)
+//        
+//        self.handleDocumentOpenURL
+//    }
+//    
+//    
+//    func handleDocumentOpenURL(url: NSURL) {
+//        let requestObj = NSURLRequest(url: url as URL)
+//        webView.isUserInteractionEnabled = true
+//        webView.loadRequest(requestObj as URLRequest)
+//    }
+//    
+//    func loadFileFromDocumentsFolder(fileName: String) {
+//        var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+//        var documentsDirectory : String;
+//        documentsDirectory = paths[0] as String
+//        let filePath = NSURL(fileURLWithPath: documentsDirectory).appendingPathComponent(fileName)
+//        
+//        self.handleDocumentOpenURL(url: filePath as! NSURL)
+//        
+//    }
     
 }
--- CustomizableCalculator/StorageVC.swift
+++ CustomizableCalculator/StorageVC.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class StorageVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    var isSlideMenuShowing = false
-    var isAddMenuShowing = false
-    let addButtonMenuItems = ["Camera", "Add file", "Add photo"]
-    let slideMenuItems = ["Photo", "Video", "Web", "Setting"]
+    // Note that your entire app’s Documents folder will be shared when you do this, including all folders you create in it. If you have data that you do not want your users to see you need to store them outside the Documents folder, or start those file names with a “full stop” (such as .testfile). This will hide the file and hence it won’t show up in iTunes.
     
     @IBOutlet weak var slideMenuLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var slideMenuTableView: UITableView!
@@ -21,6 +18,18 @@ class StorageVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
     @IBOutlet weak var addButtonTableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
 
+    var isSlideMenuShowing = false
+    var isAddMenuShowing = false
+    let addButtonMenuItems = ["Camera", "Add file", "Add photo"]
+    let slideMenuItems = ["Photo", "Video", "Web", "Setting"]
+    var photoList = [URL]()
+    var videoList = [String]()
+    
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    private var allFilesInDirectory: [URL] {
+        return try! FileManager.default.contentsOfDirectory(at: documentDirectory!, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -37,7 +46,51 @@ class StorageVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
         navigationItem.leftBarButtonItem?.tintColor = UIColor.white
         navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+        
+        photoList = allPhotosFromDirectory()
+        
     }
+    
+    fileprivate func allPhotosFromDirectory() -> [URL] {
+        return allFilesInDirectory.filter({ photo in
+            photo.pathExtension.lowercased() == "jpg" ||
+            photo.pathExtension.lowercased() == "png" ||
+            photo.pathExtension.lowercased() == "gif" ||
+            photo.pathExtension.lowercased() == "jpeg" ||
+            photo.pathExtension.lowercased() == "tiff" })
+    }
+
+    fileprivate func allVideoFromDirectory() -> [URL] {
+        return allFilesInDirectory.filter({ video in
+            video.pathExtension.lowercased() == "avi" ||
+            video.pathExtension.lowercased() == "mov" ||
+            video.pathExtension.lowercased() == "mp4" ||
+            video.pathExtension.lowercased() == "m4v" })
+    }
+    
+    
+//        var filterArray = allFiles.map(){ $0.lastPathComponent }.filter(){ $0.pathExtension == "jpg" } as [String]
+        //        println("filter array \(allFiles[3])")
+//        var superURL: NSURL!
+//        for itemInFilterArray in filterArray {
+//            var direct = fileManager.URLsForDirectory(FileManager.SearchPathDirectory.DocumentDirectory, inDomains: FileManager.SearchPathDomainMask.UserDomainMask)
+//            if var url: NSURL = direct.first as? NSURL {
+//                superURL = url.appendingPathComponent(itemInFilterArray) as NSURL!
+//            }
+//            var playerItem = AVPlayerItem(URL: superURL)
+//            var equalazerString: String!
+//            var commonMetaData = playerItem.asset.commonMetadata as! [AVMetadataItem]
+//            for item in commonMetaData {
+//                if item.commonKey == "title" {
+//                    equalazerString = item.stringValue
+//                    if equalazerString == currentSongString {
+//                        localArrayNames.append(superURL)
+//                    } else {
+//                        //                        println("it is not equals")
+//                    }
+//                }
+//            }
+//        }
     
     @IBAction func addButtonTapped(_ sender: Any) {
         if isAddMenuShowing {
@@ -101,11 +154,13 @@ class StorageVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
 //    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return photoList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mediaCell", for: indexPath) as! MediaCell
+        cell.configureCell(URL: photoList[indexPath.row].path)
         return cell
     }
     
